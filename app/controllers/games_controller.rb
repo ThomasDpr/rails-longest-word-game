d6d96c05e52fda2fd4ddaeb7f4a88652723a5b6f
Thomas Dupre <thomasdpr-pro@outlook.com>
2023-05-22 17:01:24 +0200

    snd commit

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,11 @@
+class GamesController < ApplicationController
+  def new
+    @letters = ('a'..'z').to_a.sample(10)
+  end
+
+  def score
+    # raise
+    @user_choice = params[:ask]
+    @letters = params[:letters]
+  end
+end
